32fd28e0fb599a033e21f5209fa5ba7d90349558
florintours2026-blip <florintours2026@gmail.com>
2026-09-22 14:06:31 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// Safe starter config. Replace these values during setup.
+window.NIVORA_CONFIG = {
+  SUPABASE_URL: "",
+  SUPABASE_ANON_KEY: "",
+  STORE_CURRENCY: "SAR",
+  STORE_LOCALE: "ar-SA",
+  ADMIN_EMAIL: "",
+  DEMO_MODE: true
+};
